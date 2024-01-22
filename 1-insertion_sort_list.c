d02019cc41a139de28f02b922ffc83d3e2a3b714
cox101 <cox2659@gmail.com>
2024-01-22 11:38:50 -0800

    Initial commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,55 +8,54 @@
  */
 int len_list(listint_t *h)
 {
-    int len = 0;
+	int len = 0;
 
-    while (h)
-    {
-        len++;
-        h = h->next;
-    }
-    return (len);
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
 }
 
 /**
  * insertion_sort_list - sorts a linked list with the Insertion Sort algorithm
  * @list: double pointer to the list to sort
  */
+
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current = NULL, *node_before = NULL;
-    listint_t *node_to_insert = NULL, *node_after = NULL;
 
-    if (!list || !(*list) || len_list(*list) < 2)
-        return;
+	listint_t *current = NULL, *node_before = NULL;
+	listint_t *node_to_insert = NULL, *node_after = NULL;
 
-    current = *list;
+	if (!list || !(*list) || len_list(*list) < 2)
+		return;
 
-    while (current)
-    {
-        if (current->prev && current->n < current->prev->n)
-        {
-            node_before = current->prev->prev;
-            node_to_insert = current->prev;
-            node_after = current->next;
+	current = *list;
+	while (current)
+	{
+		if (current->prev && current->n < current->prev->n)
+		{
+			node_before = current->prev->prev;
+			node_to_insert = current->prev;
+			node_after = current->next;
+			node_to_insert->next = node_after;
+			if (node_after)
+				node_after->prev = node_to_insert;
+			current->next = node_to_insert;
+			current->prev = node_before;
+			if (node_before)
+				node_before->next = current;
+			else
+				*list = current;
+			node_to_insert->prev = current;
+			current = *list;
 
-            node_to_insert->next = node_after;
-            if (node_after)
-                node_after->prev = node_to_insert;
-            current->next = node_to_insert;
-            current->prev = node_before;
-            if (node_before)
-                node_before->next = current;
-            else
-                *list = current;
-            node_to_insert->prev = current;
-            current = *list;
-
-            print_list(*list);
-            continue;
-        }
-        else
-            current = current->next;
-    }
+			print_list(*list);
+			continue;
+		}
+		else
+			current = current->next;
+	}
 }
-
