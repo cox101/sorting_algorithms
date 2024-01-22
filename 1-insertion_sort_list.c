c7f40418b956ebfa4be76d29e030cbaee2efaa0b
cox101 <cox2659@gmail.com>
2024-01-22 11:29:45 -0800

    Initial commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -19,42 +19,44 @@ int len_list(listint_t *h)
 }
 
 /**
- * insertion_sort_list - sorts a linked list with the Insert Sort algorithm
+ * insertion_sort_list - sorts a linked list with the Insertion Sort algorithm
  * @list: double pointer to the list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
+    listint_t *current = NULL, *node_before = NULL;
+    listint_t *node_to_insert = NULL, *node_after = NULL;
+
     if (!list || !(*list) || len_list(*list) < 2)
         return;
 
-    listint_t *current = *list;
+    current = *list;
 
     while (current)
     {
         if (current->prev && current->n < current->prev->n)
         {
-            listint_t *prev_prev = current->prev->prev;
-            listint_t *prev = current->prev;
-            listint_t *current_node = current;
-            listint_t *next_node = current->next;
+            node_before = current->prev->prev;
+            node_to_insert = current->prev;
+            node_after = current->next;
 
-            prev->next = next_node;
-            if (next_node)
-                next_node->prev = prev;
-            current_node->next = prev;
-            current_node->prev = prev_prev;
-            if (prev_prev)
-                prev_prev->next = current_node;
+            node_to_insert->next = node_after;
+            if (node_after)
+                node_after->prev = node_to_insert;
+            current->next = node_to_insert;
+            current->prev = node_before;
+            if (node_before)
+                node_before->next = current;
             else
-                *list = current_node;
-            prev->prev = current_node;
+                *list = current;
+            node_to_insert->prev = current;
             current = *list;
+
             print_list(*list);
+            continue;
         }
         else
-        {
             current = current->next;
-        }
     }
 }
 
