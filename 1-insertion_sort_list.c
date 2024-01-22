a3888eff1c27a7d72561035b770e4f08a07d95cf
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2024-01-22 19:00:45 +0300

    Betty Correction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -19,7 +19,7 @@ int len_list(listint_t *h)
 }
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - Sorts a doubly linked list of int in ascending order
  *                       using the Insertion Sort algorithm.
  * @list: Pointer to the head of the doubly linked list.
  */
@@ -27,32 +27,32 @@ int len_list(listint_t *h)
 void insertion_sort_list(listint_t **list);
 {
 	if (!list || !(*list) || len_list(*list) < 2)
-        return;
+		return;
 
-    listint_t *curr, *prev, *next;
+	listint_t *curr, *prev, *next;
 
-    for (curr = (*list)->next; curr; curr = next)
-    {
-        next = curr->next;
+	for (curr = (*list)->next; curr; curr = next)
+	{
+		next = curr->next;
 
-        while (curr->prev && curr->n < curr->prev->n)
-        {
-            prev = curr->prev;
-            prev->next = curr->next;
+		while (curr->prev && curr->n < curr->prev->n)
+		{
+			prev = curr->prev;
+			prev->next = curr->next;
 
-            if (curr->next)
-                curr->next->prev = prev;
+			if (curr->next)
+				curr->next->prev = prev;
 
-            curr->next = prev;
-            curr->prev = prev->prev;
-            prev->prev = curr;
+			curr->next = prev;
+			curr->prev = prev->prev;
+			prev->prev = curr;
 
-            if (curr->prev)
-                curr->prev->next = curr;
-            else
-                *list = curr;
+			if (curr->prev)
+				curr->prev->next = curr;
+			else
+				*list = curr;
 
-            print_list(*list);
-        }
-    }
+			print_list(*list);
+		}
+	}
 }
