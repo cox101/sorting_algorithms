abf28385d1f3ca41617af9fd4a407ad99422cd03
cox101 <cox2659@gmail.com>
2024-01-23 10:53:34 -0800

    Initial commit

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -8,14 +8,14 @@
  */
 size_t list_len(listint_t *list)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (list)
-    {
-        len++;
-        list = list->next;
-    }
-    return (len);
+	while (list)
+	{
+		len++;
+		list = list->next;
+	}
+	return (len);
 }
 
 /**
@@ -23,77 +23,72 @@ size_t list_len(listint_t *list)
  * @list: head of the list
  * @p: pointer to node
  */
+
 void switch_nodes(listint_t **list, listint_t **p)
 {
-    listint_t *one, *two, *three, *four;
+	listint_t *one, *two, *three, *four;	
+	
+	one = (*p)->prev;
+	two = *p;
+	three = (*p)->next;
+	four = (*p)->next->next;
 
-    one = (*p)->prev;
-    two = *p;
-    three = (*p)->next;
-    four = (*p)->next->next;
+	two->next = four;
+	if (four)
+		four->prev = two;
+	three->next = two;
+	three->prev = two->prev;
 
-    two->next = four;
-    if (four)
-        four->prev = two;
-    three->next = two;
-    three->prev = two->prev;
-
-    if (one)
-        one->next = three;
-    else
-        *list = three;
-
-    two->prev = three;
-    *p = three;
+	if (one)
+		one->next = three;
+	else
+		*list = three;
+	two->prev = three;
+	*p = three;
 }
 
 /**
- * cocktail_sort_list - sorts a doubly linked list using the cocktail sort algorithm
+ * cocktail_sort_list - sorts a doubly linked list
  * @list: pointer to list
  */
+
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *p;
-    int sorted = 0;
+	listint_t *p;
+	int sorted = 0;
 
-    if (!list || !*list || list_len(*list) < 2)
-        return;
+	if (!list || !*list || list_len(*list) < 2)
+		return;
+	p = *list;
 
-    p = *list;
-
-    while (!sorted)
-    {
-        sorted = 1;
-
-        while (p->next)
-        {
-            if (p->n > p->next->n)
-            {
-                sorted = 0;
-                switch_nodes(list, &p);
-                print_list(*list);
-            }
-            else
-                p = p->next;
-        }
-
-        if (sorted)
-            break;
-
-        p = p->prev;
-
-        while (p->prev)
-        {
-            if (p->n < p->prev->n)
-            {
-                sorted = 0;
-                p = p->prev;
-                switch_nodes(list, &p);
-                print_list(*list);
-            }
-            else
-                p = p->prev;
-        }
-    }
+	while (!sorted)
+	{
+		sorted = 1;
+		while (p->next)
+		{
+			if (p->n > p->next->n)
+			{
+				sorted = 0;
+				switch_nodes(list, &p);
+				print_list(*list);
+			}
+			else
+				p = p->next;
+		}
+		if (sorted)
+			break;
+		p = p->prev;
+		while (p->prev)
+		{
+			if (p->n < p->prev->n)
+			{
+				sorted = 0;
+				p = p->prev;
+				switch_nodes(list, &p);
+				print_list(*list);
+			}
+			else
+				p = p->prev;
+		}
+	}
 }
-
