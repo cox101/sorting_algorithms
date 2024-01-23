1e5637bd7c10db21530897c1fca3454bea9465e9
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2024-01-24 00:41:11 +0300

    A few changes on Betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,33 +9,33 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    size_t i, j, min_index;
-    int temp;
+	size_t i, j, min_index;
+	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_index = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
 
-        if (min_index != i)
-        {
-            /* Swap array[i] and array[min_index] */
-            temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
+		if (min_index != i)
+		{
+			/* Swap array[i] and array[min_index] */
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
 
-            /* Print array after each swap */
-            for (j = 0; j < size - 1; j++)
-                printf("%d, ", array[j]);
-            printf("%d\n", array[j]);
-        }
-    }
+			/* Print array after each swap */
+			for (j = 0; j < size - 1; j++)
+				printf("%d, ", array[j]);
+			printf("%d\n", array[j]);
+		}
+	}
 }
