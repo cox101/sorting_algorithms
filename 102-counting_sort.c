52e17c42c95a37af0c5044fdc34778a691f14c2d
cox101 <cox2659@gmail.com>
2024-01-23 11:43:41 -0800

    Initial  commit

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,55 +1,60 @@
 #include "sort.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * counting_sort - sorts an array using counting sort algorithm
- * @array: array to sort
- * @size: size of the array
+ * counting_sort - Sorts an array using the Counting Sort algorithm
+ * @array: Array to sort
+ * @size: Size of the array
  */
 void counting_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
+    int *count_array, *output_array, max_value, current_value, index;
+    size_t i, k, m, n;
+
+    if (size < 2)
         return;
 
     /* Find the maximum value in the array */
-    int max = array[0];
-    for (size_t i = 1; i < size; i++)
+    max_value = array[0];
+    for (i = 1; i < size; i++)
+        if (array[i] > max_value)
+            max_value = array[i];
+
+    /* Allocate memory for the counting array and output array */
+    count_array = malloc(sizeof(int) * (max_value + 1));
+    output_array = malloc(sizeof(int) * size);
+
+    /* Initialize the counting array with zeros */
+    for (k = 0; k <= max_value; k++)
+        count_array[k] = 0;
+
+    /* Populate the counting array with frequency of each element */
+    for (m = 0; m < size; m++)
     {
-        if (array[i] > max)
-            max = array[i];
+        current_value = array[m];
+        count_array[current_value]++;
     }
-
-    /* Create and initialize the counting array */
-    int *counting_array = malloc((max + 1) * sizeof(int));
-    if (counting_array == NULL)
-        return;
-
-    for (int i = 0; i <= max; i++)
-        counting_array[i] = 0;
-
-    /* Populate the counting array */
-    for (size_t i = 0; i < size; i++)
-        counting_array[array[i]]++;
 
     /* Print the counting array */
-    printf("Counting array:");
-    for (int i = 0; i <= max; i++)
-        printf(" %d", counting_array[i]);
-    printf("\n");
+    print_array(count_array, max_value + 1);
 
-    /* Update the original array with sorted values */
-    size_t j = 0;
-    for (int i = 0; i <= max; i++)
+    /* Modify the counting array to store cumulative frequencies */
+    for (n = 1; n <= max_value; n++)
+        count_array[n] += count_array[n - 1];
+
+    /* Build the output array based on the counting array */
+    for (i = 0; i < size; i++)
     {
-        while (counting_array[i] > 0)
-        {
-            array[j++] = i;
-            counting_array[i]--;
-        }
+        index = count_array[array[i]] - 1;
+        output_array[index] = array[i];
+        count_array[array[i]]--;
     }
 
-    /* Free the counting array */
-    free(counting_array);
+    /* Copy the sorted elements back to the original array */
+    for (i = 0; i < size; i++)
+        array[i] = output_array[i];
+
+    /* Free allocated memory */
+    free(count_array);
+    free(output_array);
 }
 
