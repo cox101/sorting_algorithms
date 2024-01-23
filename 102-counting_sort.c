d6192b8088be2030ca534e46707a7241cb3b7cf7
cox101 <cox2659@gmail.com>
2024-01-23 11:59:24 -0800

    Initial  commit

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,59 +1,50 @@
 #include "sort.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * counting_sort - sorts an array using the Counting Sort algorithm
- * @array: array to sort
+ * counting_sort - sorts an array using the Counting sort algorithm
+ * @array: array to be sorted
  * @size: size of the array
  */
 void counting_sort(int *array, size_t size)
 {
+    int *count_arr, *out_arr, max, num;
+    size_t i, k, m, n;
+
     if (size < 2)
         return;
 
-    int *count_array, *output_array, max;
-
-    /* Find the maximum value in the array */
     max = array[0];
-    for (size_t i = 1; i < size; i++)
-    {
+    for (i = 1; i < size; i++)
         if (array[i] > max)
             max = array[i];
-    }
 
-    /* Allocate memory for counting and output arrays */
-    count_array = malloc((max + 1) * sizeof(int));
-    output_array = malloc(size * sizeof(int));
+    count_arr = malloc(sizeof(int) * (max + 1));
+    out_arr = malloc(sizeof(int) * size);
 
-    /* Initialize counting array to zero */
-    for (int j = 0; j <= max; j++)
-        count_array[j] = 0;
+    for (i = 0; i <= max; i++)
+        count_arr[i] = 0;
 
-    /* Count occurrences of each element in the array */
-    for (size_t k = 0; k < size; k++)
-        count_array[array[k]]++;
-
-    /* Print the counting array */
-    print_array(count_array, max + 1);
-
-    /* Modify counting array to represent positions in the output array */
-    for (int l = 1; l <= max; l++)
-        count_array[l] += count_array[l - 1];
-
-    /* Build the sorted output array */
-    for (size_t m = 0; m < size; m++)
+    for (k = 0; k < size; k++)
     {
-        output_array[count_array[array[m]] - 1] = array[m];
-        count_array[array[m]]--;
+        num = array[k];
+        count_arr[num] += 1;
     }
 
-    /* Copy the sorted output array back to the original array */
-    for (size_t n = 0; n < size; n++)
-        array[n] = output_array[n];
+    for (i = 1; i <= max; i++)
+        count_arr[i] += count_arr[i - 1];
 
-    /* Free allocated memory */
-    free(count_array);
-    free(output_array);
+    print_array(count_arr, max + 1);
+
+    for (m = 0; m < size; m++)
+    {
+        out_arr[count_arr[array[m]] - 1] = array[m];
+        count_arr[array[m]]--;
+    }
+
+    for (n = 0; n < size; n++)
+        array[n] = out_arr[n];
+
+    free(count_arr);
+    free(out_arr);
 }
 
