89824123385f435abed28443b4ceaba412be7101
cox101 <cox2659@gmail.com>
2024-01-27 09:29:40 -0800

    Initial commit

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,69 +1,74 @@
 #include "sort.h"
 
 /**
- * quick_sort_hoare - sorts an array using the Quicksort algorithm with Hoare partition
+ * quick_sort_hoare - sorts an array using the Quicksort algorithm (Hoare)
  * @array: array of integers to sort
  * @size: size of the array
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (size < 2)
-		return;
+    if (size < 2)
+        return;
 
-	quick_sort_recursive(array, 0, (int)size - 1, size);
+    quicksort_hoare(array, 0, (int)size - 1, size);
 }
 
 /**
- * quick_sort_recursive - recursive helper function for Quicksort (Hoare partition)
+ * quicksort_hoare - recursive helper function for Quicksort (Hoare)
  * @array: array to sort
- * @left: index of the leftmost element
- * @right: index of the rightmost element
+ * @left: index of the left element
+ * @right: index of the right element
  * @size: size of the array
  */
-void quick_sort_recursive(int *array, int left, int right, size_t size)
+void quicksort_hoare(int *array, int left, int right, size_t size)
 {
-	int pivot_index;
+    int pivot;
 
-	if (left < right)
-	{
-		pivot_index = hoare_partition(array, left, right, size);
-		quick_sort_recursive(array, left, pivot_index, size);
-		quick_sort_recursive(array, pivot_index + 1, right, size);
-	}
+    if (left < right)
+    {
+        pivot = hoare_partition(array, left, right, size);
+        quicksort_hoare(array, left, pivot, size);
+        quicksort_hoare(array, pivot + 1, right, size);
+    }
 }
 
 /**
  * hoare_partition - performs the Hoare partition scheme for Quicksort
  * @array: array to partition
- * @left: index of the leftmost element
- * @right: index of the rightmost element
+ * @left: starting index of the partition
+ * @right: ending index of the partition
  * @size: size of the array
  * Return: index of the pivot after partition
  */
 int hoare_partition(int *array, int left, int right, size_t size)
 {
-	int tmp, pivot = array[right];
-	size_t i = (size_t)left - 1, j = (size_t)right + 1;
+    int tmp, pivot = array[right];
+    int i = left - 1;
+    int j = right + 1;
 
-	while (1)
-	{
-		do {
-			i++;
-		} while (array[i] < pivot);
+    while (1)
+    {
+        do
+        {
+            i++;
+        } while (array[i] < pivot);
 
-		do {
-			j--;
-		} while (array[j] > pivot);
+        do
+        {
+            j--;
+        } while (array[j] > pivot);
 
-		if (i >= j)
-			return (i);
+        if (i >= j)
+            return i;
 
-		if (i != j)
-		{
-			tmp = array[i];
-			array[i] = array[j];
-			array[j] = tmp;
-			print_array(array, size);
-		}
-	}
+        if (i != j)
+        {
+            tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+            print_array(array, size);
+        }
+    }
+    return 0;
 }
+
