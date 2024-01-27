2f635e5682be83c2f7f0564b3140349b400489e5
cox101 <cox2659@gmail.com>
2024-01-27 08:48:40 -0800

    Initial commit

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -5,20 +5,22 @@
  * @array: Array of integers to sort
  * @size: Size of the array
  */
+
 void merge_sort(int *array, size_t size)
 {
-    int *temp_array;
+	int *temp_array;
 
-    if (!array || size < 2)
-        return;
 
-    temp_array = malloc(sizeof(int) * size);
+	if (!array || size < 2)
+		return;
 
-    if (temp_array == NULL)
-        return;
+	temp_array = malloc(sizeof(int) * size);
 
-    merge_recursion(temp_array, array, 0, size);
-    free(temp_array);
+	if (temp_array == NULL)
+		return;
+
+	merge_recursion(temp_array, array, 0, size);
+	free(temp_array);
 }
 
 /**
@@ -28,17 +30,20 @@ void merge_sort(int *array, size_t size)
  * @left: Index of the left element
  * @right: Index of the right element
  */
+
 void merge_recursion(int *temp_array, int *array, size_t left, size_t right)
 {
-    size_t middle;
+	size_t middle;
 
-    if (right - left > 1)
-    {
-        middle = (right - left) / 2 + left;
-        merge_recursion(temp_array, array, left, middle);
-        merge_recursion(temp_array, array, middle, right);
-        merge_subarray(temp_array, array, left, middle, right);
-    }
+
+	if (right - left > 1)
+	{
+		middle = (right - left) / 2 + left;
+		merge_recursion(temp_array, array, left, middle);
+		merge_recursion(temp_array, array, middle, right);
+		merge_subarray(temp_array, array, left, middle, right);
+
+	}
 }
 
 /**
@@ -49,34 +54,34 @@ void merge_recursion(int *temp_array, int *array, size_t left, size_t right)
  * @middle: Index of the middle element
  * @right: Index of the right element
  */
+
 void merge_subarray(int *temp_array, int *array, size_t left,
                     size_t middle, size_t right)
 {
-    size_t i, j, k = 0;
+	size_t i, j, k = 0;
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array + left, middle - left);
+	printf("[right]: ");
+	print_array(array + middle, right - middle);
 
-    printf("Merging...\n");
-    printf("[left]: ");
-    print_array(array + left, middle - left);
-    printf("[right]: ");
-    print_array(array + middle, right - middle);
+	for (i = left, j = middle; i < middle && j < right; k++)
+	{
+		if (array[i] < array[j])
+			temp_array[k] = array[i++];
 
-    for (i = left, j = middle; i < middle && j < right; k++)
-    {
-        if (array[i] < array[j])
-            temp_array[k] = array[i++];
-        else
-            temp_array[k] = array[j++];
-    }
+		else
+			temp_array[k] = array[j++];
+	}
 
-    while (i < middle)
-        temp_array[k++] = array[i++];
-    while (j < right)
-        temp_array[k++] = array[j++];
+	while (i < middle)
+		temp_array[k++] = array[i++];
+	while (j < right)
+		temp_array[k++] = array[j++];
 
-    for (k = left, i = 0; k < right; k++)
-        array[k] = temp_array[i++];
+	for (k = left, i = 0; k < right; k++)
+		array[k] = temp_array[i++];
 
-    printf("[Done]: ");
-    print_array(array + left, right - left);
+	printf("[Done]: ");
+	print_array(array + left, right - left);
 }
-
